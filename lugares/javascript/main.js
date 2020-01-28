8bed6c693e1bc4762dfb1a004d2486159454849f
Génesis Rosales <gene2708@gmail.com>
2020-01-28 14:06:13 -0400

    Proyecto de lugares

---
+++ lugares/javascript/main.js
@@ -0,0 +1,25 @@
+//Obtener los elementos de la clase .close
+let links =  document.querySelectorAll(".close");
+
+//Recorrer los elementos
+links.forEach(function(link){
+    //Agregar un evento click a cada uno de ellos
+    link.addEventListener('click', function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+    
+        //Quitarle las clases de animación que ya tiene
+        content.classList.remove("zoomIn");
+        content.classList.remove("animated");
+
+        //Agregar clases para animar su salida
+        content.classList.add("zoomOut");
+        content.classList.add("animated");
+
+        setTimeout(function(){
+            location.href = "/";
+        },600);
+
+        return false;
+    });
+});
